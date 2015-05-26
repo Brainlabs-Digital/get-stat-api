fad78c64cf0ef15f9145b89767a893e7fae05b97
Austin <austin.platt@distilled.co.uk>
2015-05-26 10:21:40 +0100

    add more comments

--- stat_api/stat_api.py
+++ stat_api/stat_api.py
@@ -3,14 +3,36 @@ import time
 import requests
 
 ENDPOINTS_DATA = {
+    # projects
     "/projects/list": (),
     "/projects/create": (),
     "/projects/update": (),
     "/projects/delete": (),
+    # sites
+    "/sites/all": (),
     "/sites/list": (),
+    "/sites/ranking_distributions": (),
+    "/sites/create": (),
+    "/sites/update": (),
+    "/sites/delete": (),
+    # tags
+    "/tags/list": (),
+    "/tags/ranking_distributions": (),
+    # keywords
+    "/keywords/list": (),
+    "/keywords/create": (),
+    "/keywords/delete": (),
+    # rankings
+    "/rankings/list": (),
+    # serps
+    "/serps/show": (),
+    # bulk jobs
     "/bulk/list": (),
+    "/bulk/ranks": (),
     "/bulk/status": (),
-    "/bulk/site_ranking_distributions": ()
+    "/bulk/delete": (),
+    "/bulk/site_ranking_distributions": (),
+    "/bulk/tag_ranking_distributions": (),
 }
 
 class StatInvalidEndpoint(Exception):
@@ -21,16 +43,21 @@ class StatRequestError(Exception):
 
 class Stat(object):
 
-    def __init__(self, api_key):
+    def __init__(self, subdomain, api_key):
 
-        self.base_url =  'http://app4.getstat.com/'
+        self.base_url =  self._make_base_url(subdomain)
         self.api_key = api_key
 
-    def make_api_request_url(self, endpoint):
+    def _make_base_url(self, subdomain):
 
-        return self.base_url + "api/v2/" + self.api_key + endpoint
+        return "http://" + subdomain + ".getstat.com"
 
-    def do_request(self, url, kwargs):
+    def _make_api_request_url(self, endpoint):
+
+        return self.base_url + "/api/v2/" + self.api_key + endpoint
+
+    def _do_request(self, url, kwargs):
+        """ Execute a request """
 
         r = requests.get(url, kwargs)
 
@@ -49,52 +76,68 @@ class Stat(object):
         return r.json()['Response']
 
     def request(self, endpoint, **kwargs):
+        """ Make a request to the getstat.com API
+
+        endpoint should correspond to an endpoint listed in the documentation
+        kawrgs should be a dictionary of query parameters for the request
+        """
 
         if not endpoint in ENDPOINTS_DATA.keys():
             raise InvalidEndpoint("The endpoint {endpoint} does not exist".format(endpoint))
 
-        url = self.make_api_request_url(endpoint)
+        url = self._make_api_request_url(endpoint)
         kwargs.update({'format': 'json'})
 
-        return self.do_request(url, kwargs)
+        return self._do_request(url, kwargs)
 
-    def make_report_stream_url(self, job_id):
+    def _make_report_stream_url(self, job_id):
 
         return self.base_url + "/bulk_reports/stream_report/" + str(job_id)
 
     def get_job_result(self, job_id):
+        """ Get the JSON of a report associated with the given job_id"""
 
-        url = self.make_report_stream_url(job_id)
+        url = self._make_report_stream_url(job_id)
         params = {"key": self.api_key}
-        return self.do_request(url, params)
+        return self._do_request(url, params)
 
     def get_job_status(self, job_id):
+        """ Return the status of a job """
 
         params = {"id": job_id}
         return self.request("/bulk/status", **params)['Result']['Status']
 
     def is_job_done(self, job_id):
+        """ Has a job finished executing and the report is ready """
 
         return self.get_job_status(job_id) == 'Completed'
 
 
 class StatBulkJob(object):
+    """ Helper class to deal with running jobs on STAT """
 
-    def __init__(self, api_key):
+    def __init__(self, subdomain, api_key):
 
-        self.stat = Stat(api_key)
+        self.stat = Stat(subdomain, api_key)
 
     def create_job(self, endpoint, **kwargs):
+        """ Make a job and return the Id associated with it """
 
         result = self.stat.request(endpoint, **kwargs)
         job_id = result['Result']['Id']
         return job_id
 
-    def create_job_and_wait_for_result(self, endpoint, **kwargs):
+    def create_job_and_wait_for_result(self, endpoint, time_interval=5, **kwargs):
+        """ Make a job and wait for the result (by polling for the job status)
+
+        Optionally, set the length of the period to wait between checking if
+        the job is done.
+        """
 
         job_id = self.create_job(endpoint, **kwargs)
+
         while True:
-            time.sleep(5)
+            time.sleep(time_interval)
             if self.stat.is_job_done(job_id):
                 break
 
