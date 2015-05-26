8a1e59c552ba3f3ecd7081881aab72d3c80c29f7
Austin <austin.platt@distilled.co.uk>
2015-05-26 14:46:30 +0100

    increase version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'stat_api',
   packages = ['stat_api'], # this must be the same as the name above
-  version = '0.2',
+  version = '0.3',
   description = 'A wrapper for the getstat.com API',
   author = 'Austin Platt',
   author_email = 'austin.platt@distilled.net',
   url = 'https://github.com/DistilledLtd/get-stat-api', # use the URL to the github repo
-  download_url = 'https://github.com/DistilledLtd/get-stat-api/tarball/0.2', # I'll explain this in a second
+  download_url = 'https://github.com/DistilledLtd/get-stat-api/tarball/0.3', # I'll explain this in a second
   keywords = ['stat', 'getstat', 'api', 'keyword'], # arbitrary keywords
   classifiers = [],
 )
